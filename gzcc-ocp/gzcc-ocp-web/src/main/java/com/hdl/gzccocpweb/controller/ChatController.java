7225ebe957f38d18cb34e14dfbdb4f8e3673bb72
hudelin <736583887@qq.com>
2018-12-29 17:17:53 +0800

    添加webSocket连接

--- gzcc-ocp/gzcc-ocp-web/src/main/java/com/hdl/gzccocpweb/controller/ChatController.java
+++ gzcc-ocp/gzcc-ocp-web/src/main/java/com/hdl/gzccocpweb/controller/ChatController.java
@@ -1,12 +1,15 @@
 package com.hdl.gzccocpweb.controller;
 
+import com.hdl.gzccocpweb.webSocket.WebSocketServer;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 @RequestMapping("/chat")
@@ -191,4 +194,24 @@ public class ChatController {
                 "  }\n" +
                 "}";
     }
+
+    @RequestMapping("/socket/{cid}")
+    public ModelAndView socket(@PathVariable String cid) {
+        ModelAndView mav=new ModelAndView("/socket");
+        mav.addObject("cid", cid);
+        return mav;
+    }
+    //推送数据接口
+    @ResponseBody
+    @RequestMapping("/socket/push/{cid}")
+    public void pushToWeb(@PathVariable String cid,String message) {
+        try {
+            WebSocketServer.sendInfo(message,cid);
+        } catch (IOException e) {
+            e.printStackTrace();
+//            return ApiReturnUtil.error(cid+"#"+e.getMessage());
+        }
+//        return ApiReturnUtil.success(cid);
+    }
+
 }
